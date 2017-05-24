f92b0aa513b57bf8e85d0418f6c2b97c3e641530
gankkank <gankkank@gmail.com>
2017-05-24 18:24:30 +0800

    update help page and readme

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # README
 
+Install use:
+
+`yarn global add gitlab-update`
+
+Or:
+
+`npm install gitlab-update -g`
+
+## Help
+
 Sync projects in namespace to local folders
 
 ```txt
+
   Usage: gitlab-update [options]
 
   Options:
@@ -12,7 +23,15 @@ Sync projects in namespace to local folders
     -t, --token [token]          Set token, Required
     -n, --namespace [namespace]  Set namespace, Required
     -u, --url [url]              Set gitlab url, Default: https://gitlab.com
-    -d, --dir [dir]              Set target directory, Default: $HOME/tmp
+    -d, --dir [dir]              Set target directory, Default: /Users/jimmy/tmp
     -l, --list                   List projects
+
+  Examples:
+
+  Clone/Update projects in namespace:
+    $ gitlab-update -t xxxx -n xxx
+  List projects in certain namespace:
+    $ gitlab-update -t xxxx -n xxx -l
+
 
 ```
--- src/parser.ts
+++ src/parser.ts
@@ -9,17 +9,16 @@ program
     .option('-u, --url [url]', `Set gitlab url, Default: ${config.url}`)
     .option('-d, --dir [dir]', `Set target directory, Default: ${config.dir}`)
     .option('-l, --list', `List projects`)
+    .on('--help', function () {
+        console.log('  Examples:');
+        console.log('');
+        console.log('  Clone/Update projects in namespace:');
+        console.log('    $ gitlab-update -t xxxx -n xxx');
+        console.log('  List projects in certain namespace:');
+        console.log('    $ gitlab-update -t xxxx -n xxx -l');
+        console.log('');
+    })
     .parse(process.argv);
-
-program.on('--help', function () {
-    console.log('  Examples:');
-    console.log('');
-    console.log('  Clone/Update projects in namespace:');
-    console.log('    $ custom -t xxxx -n xxx');
-    console.log('  List projects in certain namespace:');
-    console.log('    $ custom -t xxxx -n xxx -l');
-    console.log('');
-});
 
 
 if (program.token === undefined || program.namespace === undefined) {
